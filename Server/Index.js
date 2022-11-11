eed86c35305bbbdd9922702dae86bebe974305ef
Clive tsungu <harmonytsungu@gmail.com>
2022-11-11 15:17:38 +0000

    Created backend

---
+++ Server/Index.js
@@ -0,0 +1,13 @@
+import dbConn from "./config/Dbconn";
+import app from './routes/routes';
+import dotenv from 'dotenv';
+
+dbConnect();
+dotenv.config()
+
+const aPORT =  process.env.PORT;
+
+
+app.listen(aPORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${aPORT}`);
+});
