b894a759219aa331bb317875a8228ba0b07082ee
Darren Charlton <darren.charlton@tui.co.uk>
2022-11-14 12:33:13 +0000

    myskills popup

--- src/components/handylanding/myskills.js
+++ src/components/handylanding/myskills.js
@@ -5,6 +5,7 @@ import plumbing from '../../assets/plumbing.png'
 import tiling from '../../assets/Tiling.png'
 import Popup from 'reactjs-popup'
 
+
 const skills = () => {
     return (
         
@@ -15,7 +16,23 @@ const skills = () => {
         <img className='w-1/12' src={plumbing} alt="bathroom" />
         <img className='w-1/12' src={tiling} alt="bathroom" />
         <Popup trigger= { <button className='text-white py-1 bg-[#606060] px-5 border rounded-xl hover:bg-[#fccc35] hover:scale-110'>Add / Remove</button>
-        }> </Popup>
+            }
+            on="click"
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: '0px', border: 'none' }}
+            arrow={false}
+            position="top right"
+            > 
+            <div className="menu">
+            <div className="menu-item"> item 1</div>
+            <div className="menu-item"> item 2</div>
+            <div className="menu-item"> item 3</div>
+            </div>
+        <span className='bg-white rounded-x1 '>this is where the content will go</span>    
+            
+        </Popup>
     </div>
 
 
