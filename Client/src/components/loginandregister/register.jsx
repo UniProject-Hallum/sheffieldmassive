c9f7dd16ff2e31ad1c6c64d2c0a4f188c7976793
Clive tsungu <harmonytsungu@gmail.com>
2022-11-15 22:39:34 +0000

    a

--- Client/src/components/loginandregister/register.jsx
+++ Client/src/components/loginandregister/register.jsx
@@ -40,11 +40,7 @@ const Register = () => {
       function handleSelectCity(data) { 
         setSelectedCity(data);
       }
-
-
-     
       
-
     const OnClickSignup = async () => {
     try {
         let itoken;
@@ -52,8 +48,7 @@ const Register = () => {
         let values= [];
         selectedOptions.forEach(obj => { values.push(obj['value']) })
         
-
-        let userinfo = {
+       let userinfo = {
             description: descriptionValue,
             gender: genderValue,
             skills: values,
@@ -61,7 +56,6 @@ const Register = () => {
         };
 
         console.log("this is user info :  "+ JSON.stringify(userinfo));
-
         const response = await axios.post(baseurl+'/api/register',
             { email:emailValue,
                 username:usernameValue,
@@ -91,20 +85,16 @@ const Register = () => {
             <div className='md:w-1/2 px-16'>
                 <h2 className='font-bold text-2xl text-[#606060]'>Register</h2>
                 <p className='text-sm mt-4 text-[#606060]'>You aren't a memeber, easily register</p>
-
                 {errorMessage&& <div className='fail p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800' >{errorMessage}</div>}
-
                 <div  className='flex flex-col gap-4'>
                     <input required className='p-2 mt-8 rounded-xl border' type="test" name='name' placeholder='Full Name' onChange={e => setUsernameValue(e.target.value)}/>
                     <input required className='p-2 rounded-xl border' type="email" name='email' placeholder='Email' onChange={e => setEmailValue(e.target.value)}/>
-                    
-                    <div className='relative'>
+                                        <div className='relative'>
                         <input required className='p-2 rounded-xl border w-full' type="password" name='password' placeholder='Password'onChange={e => setPasswordValue(e.target.value)} />
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey"  className=' bi bi-eye absolute top-1/2 right-3 -translate-y-1/2' viewBox="0 0 16 16">
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                        </svg>
-                        
+                        </svg>                        
                     </div>
                     <div className='flex'>
                         <div className="flex items-center mr-4">
@@ -114,11 +104,8 @@ const Register = () => {
                         <div className="flex items-center mr-4">
                             <input required onChange={e => setRoleValue('handyman')}   id="inline-radio2" type="radio" value="" name="inline-radio-group" className="w-4 h-4  bg-gray-100 border-gray-300  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                             <label htmlFor="inline-radio2" className="ml-2 text-sm font-medium text-[#606060] dark:text-gray-300">HandyMan</label>
-                        </div>
-
-                       
+                        </div>                       
                     </div>
-
                     {
                           roleValue ==='handyman' &&(
                             <div>        
